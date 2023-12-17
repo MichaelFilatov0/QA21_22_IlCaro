b0fc8b5e49eb976fcc14964dba3d9533c44ea968
MichaelFilatov0 <118763960+MichaelFilatov0@users.noreply.github.com>
2023-12-17 02:50:46 +0200

    LogRegPositiveTest

--- Qa21IlCaro/src/main/java/manager/HelperUser.java
+++ Qa21IlCaro/src/main/java/manager/HelperUser.java
@@ -1,9 +1,21 @@
 package manager;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 
 public class HelperUser extends HelperBase{
     public HelperUser(WebDriver wd) {
         super(wd);
+    }
+    public void openLogRegForm(){
+        WebElement logform= wd.findElement(By.xpath("//a[@class='navigation-link'][5]"));
+        logform.click();
+    }
+    public void typeLogPass(String email, String password){
+        type(By.id("email"),email);
+        type(By.id("password"),password);
+        click(By.cssSelector("button[type='submit']"));
+        click(By.cssSelector("button[type='button']"));
     }
 }
--- Qa21IlCaro/src/test/java/tests/LoginTests.java
+++ Qa21IlCaro/src/test/java/tests/LoginTests.java
@@ -5,6 +5,8 @@ import org.testng.annotations.Test;
 public class LoginTests extends TestBase{
     @Test
     public void test(){
+    app.getHelperUser().openLogRegForm();
+    app.getHelperUser().typeLogPass("a@gmail.com","Mm2756808@");
 
     }
 }
